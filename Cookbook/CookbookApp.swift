f94e88d11a1b953401c28c23995221ee883c53f4
Aziz Kızgın <azizkzgn@gmail.com>
2024-01-18 23:13:45 +0300

    Initial Commit

---
+++ Cookbook/CookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookbookApp.swift
+//  Cookbook
+//
+//  Created by Aziz Kızgın on 18.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct CookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
