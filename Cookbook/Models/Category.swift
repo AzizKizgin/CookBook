10abf42f9aaeac07f2a85e58a7d7a010f06a055b
Aziz Kızgın <azizkzgn@gmail.com>
2024-01-20 20:37:04 +0300

    feat: Category model added

---
+++ Cookbook/Models/Category.swift
@@ -0,0 +1,22 @@
+//
+//  Category.swift
+//  Cookbook
+//
+//  Created by Aziz Kızgın on 19.01.2024.
+//
+
+import Foundation
+
+struct Category: Codable {
+    let id: String
+    let name: String
+    let image: String
+    let description: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "idCategory"
+        case name = "strCategory"
+        case image = "strCategoryThumb"
+        case description = "strCategoryDescription"
+    }
+}
