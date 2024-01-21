23752fa18c6e744a782dd4c785676d7fa68ff6fe
Aziz Kızgın <azizkzgn@gmail.com>
2024-01-21 19:05:10 +0300

    feat: CategoryDataType added

---
+++ Cookbook/Models/CategoryDataType.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryDataType.swift
+//  Cookbook
+//
+//  Created by Aziz Kızgın on 21.01.2024.
+//
+
+import Foundation
+
+struct CategoryDataType: Decodable {
+    let categories: [Category]
+}
