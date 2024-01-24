2924fe2441aebe88482d14cb6ae602164bb98457
Aziz Kızgın <azizkzgn@gmail.com>
2024-01-24 19:54:14 +0300

    feat: FlipCard updated for customization

--- Cookbook/Commons/FlipCard.swift
+++ Cookbook/Commons/FlipCard.swift
@@ -9,29 +9,54 @@ import Foundation
 import SwiftUI
 
 private struct CardFront : View {
-    let width : CGFloat
-    let height : CGFloat
     @Binding var degree : Double
+    let width: CGFloat
+    let height: CGFloat
+    let imageHeight: CGFloat
+    let url: String
+    let showButton: Bool
+    let buttonTitle: String?
+    let onButtonPress: () -> Void
+    
+    init(degree: Binding<Double>, width: CGFloat, height: CGFloat, url: String, showButton: Bool = false, buttonTitle: String? = nil, onButtonPress: @escaping () -> Void = {}) {
+        self._degree = degree
+        self.width = width
+        self.height = height
+        self.url = url
+        self.showButton = showButton
+        self.buttonTitle = buttonTitle
+        self.imageHeight = showButton ? height - 50: height
+        self.onButtonPress = onButtonPress
+    }
 
     var body: some View {
             VStack(spacing:0){
-                AsyncImage(url: URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/300px-Good_Food_Display_-_NCI_Visuals_Online.jpg")){ image in
+                AsyncImage(url: URL(string: url)){ image in
                     image
                         .resizable()
-                        .frame(width: width, height: height - 50)
-                        .aspectRatio(contentMode: .fit)
-                        .clipShape(UnevenRoundedRectangle(topLeadingRadius: 20, topTrailingRadius: 20))
-                        .shadow(color: .gray, radius: 2, x: 0, y: 0)
                 } placeholder: {
                     ProgressView()
                         .controlSize(.large)
-                        .frame(width: width, height: height - 50)
                 }
-                Button(action: {}, label: {
-                    Text("See Meals")
-                        .frame(height: 50)
-                        .frame(maxWidth: .infinity)
-                })
+                .frame(width: width, height: imageHeight)
+                .clipShape(
+                    UnevenRoundedRectangle(
+                        topLeadingRadius: 20,
+                        bottomLeadingRadius: showButton ? 0 : 20,
+                        bottomTrailingRadius: showButton ? 0 : 20,
+                        topTrailingRadius: 20
+                    )
+                )
+                .aspectRatio(contentMode: .fit)
+                .shadow(color: .gray, radius: 2, x: 0, y: 0)
+                
+                if showButton {
+                    Button(action: onButtonPress, label: {
+                        Text(buttonTitle ?? "See All")
+                            .frame(height: 50)
+                            .frame(maxWidth: .infinity)
+                    })
+                }
             }
             .frame(width: width, height: height)
             .background(
@@ -45,13 +70,14 @@ private struct CardFront : View {
 }
 
 private struct CardBack : View {
+    @Binding var degree : Double
     let width : CGFloat
     let height : CGFloat
-    @Binding var degree : Double
+    let text: String
 
     var body: some View {
             ScrollView{
-                Text("Vegetarianism is the practice of abstaining from the consumption of meat (red meat, poultry, seafood, and the flesh of any other animal), and may also include abstention from by-products of animal slaughter. Vegetarianism may be adopted for various reasons. Many people object to eating meat out of respect for sentient life. Such ethical motivations have been codified under various religious beliefs, as well as animal rights advocacy. Other motivations for vegetarianism are health-related, political, environmental, cultural, aesthetic, economic, or personal preference. There are variations of the diet as well: an ovo-lacto vegetarian diet includes both eggs and dairy products, an ovo-vegetarian diet includes eggs but not dairy products, and a lacto-vegetarian diet includes dairy products but not eggs. A vegan diet excludes all animal products, including eggs and dairy. Some vegans also avoid other animal products such as beeswax, leather or silk clothing, and goose-fat shoe polish.")
+                Text(text)
                     .padding()
             }
             .background(
@@ -67,14 +93,30 @@ private struct CardBack : View {
 
 struct FlipCard: View {
     //MARK: Variables
+    let url: String
+    let showButton: Bool
+    let buttonTitle: String?
+    let onButtonPress: () -> Void
+    let backText: String
+    let width : CGFloat
+    let height : CGFloat
+    let durationAndDelay : CGFloat
+    
+    init(url: String, showButton: Bool = false, buttonTitle: String? = nil, onButtonPress: @escaping () -> Void = {}, backText: String = "", width: CGFloat = 300, height: CGFloat = 375, delay: CGFloat = 0.3) {
+        self.url = url
+        self.showButton = showButton
+        self.buttonTitle = buttonTitle
+        self.onButtonPress = onButtonPress
+        self.backText = backText
+        self.width = width
+        self.height = height
+        self.durationAndDelay = delay
+    }
+    
     // if numbers here are 0.00 and 90.0 it causes "ignoring singular matrix" error. so i used this weird numbers
     @State private var frontDegree = 0.00001
     @State private var backDegree = -89.999
     @State private var isFlipped = false
-
-    let width : CGFloat = 300
-    let height : CGFloat = 375
-    let durationAndDelay : CGFloat = 0.3
 
     //MARK: Flip Card Function
     func flipCard () {
@@ -99,8 +141,8 @@ struct FlipCard: View {
     //MARK: View Body
     var body: some View {
         ZStack {
-            CardFront(width: width, height: height, degree: $frontDegree)
-            CardBack(width: width, height: height, degree: $backDegree)
+            CardFront(degree: $frontDegree, width: width, height: height, url: url, showButton: showButton, buttonTitle: buttonTitle, onButtonPress: onButtonPress)
+            CardBack(degree: $backDegree, width: width, height: height, text: backText)
         }
         .onTapGesture {
             flipCard()
@@ -109,5 +151,5 @@ struct FlipCard: View {
 }
 
 #Preview{
-    FlipCard()
+    FlipCard(url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/300px-Good_Food_Display_-_NCI_Visuals_Online.jpg", showButton: true, buttonTitle: "See Meals", onButtonPress: {print("dddd")}, backText: "Veganism is both the practice of abstaining from the use of animal products, particularly in diet, and an associated philosophy that rejects the commodity status of animals.[b] A follower of either the diet or the philosophy is known as a vegan (pronounced /ˈviːɡən/ VEE-gən). Distinctions are sometimes made between several categories of veganism. Dietary vegans (or strict vegetarians) refrain from consuming animal products, not only meat but also eggs, dairy products and other animal-derived substances.[c] The term ethical vegan is often applied to those who not only follow a vegan diet but extend the philosophy into other areas of their lives, and oppose the use of animals for any purpose.[d] Another term is environmental veganism, which refers to the avoidance of animal products on the premise that the harvesting or industrial farming of animals is environmentally damaging and unsustainable.")
 }
