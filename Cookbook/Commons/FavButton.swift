2bb6cfd0cd6e6e2d9c1703f9ed6240115b22d41c
Aziz Kızgın <azizkzgn@gmail.com>
2024-01-25 22:38:42 +0300

    feat: FavButton file added

---
+++ Cookbook/Commons/FavButton.swift
@@ -0,0 +1,18 @@
+//
+//  FavButton.swift
+//  Cookbook
+//
+//  Created by Aziz Kızgın on 25.01.2024.
+//
+
+import SwiftUI
+
+struct FavButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavButton()
+}
