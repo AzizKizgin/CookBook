eba5a603471bb382dc5db7603ea59fdc99dc0bb7
Aziz Kızgın <azizkzgn@gmail.com>
2024-01-29 01:31:30 +0300

    feat: navigationTitle removed

--- Cookbook/Views/Home/HomeView.swift
+++ Cookbook/Views/Home/HomeView.swift
@@ -39,7 +39,7 @@ struct HomeView: View {
                 }
             }
         }
-        .navigationTitle("Categories")
+      
         .onAppear{
             homeVM.getCategories()
         }
