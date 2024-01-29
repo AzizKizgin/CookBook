1fe32f08eef235911b9b637f341cdfa700a1fb8f
Aziz Kızgın <azizkzgn@gmail.com>
2024-01-30 01:58:45 +0300

    feat: SearchBar added

---
+++ Cookbook/Commons/SearchBar.swift
@@ -0,0 +1,67 @@
+//
+//  SearchBar.swift
+//  Cookbook
+//
+//  Created by Aziz Kızgın on 30.01.2024.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    @Binding var text: String
+
+    @State private var isEditing = false
+        
+    var body: some View {
+        HStack {
+            TextField("Search...", text: $text)
+                .padding(8)
+                .padding(.horizontal, 25)
+                .background(.gray.opacity(0.15))
+                .cornerRadius(10)
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 8)
+                        
+                        if isEditing && !text.isEmpty {
+                            Button(action: {
+                                self.text = ""
+                            }) {
+                                Image(systemName: "multiply.circle.fill")
+                                    .foregroundColor(.gray)
+                                    .padding(.trailing, 8)
+                            }
+                        }
+                    }
+                )
+                .padding(.horizontal, 10)
+                .onTapGesture {
+                    self.isEditing = true
+                }
+            
+            if isEditing {
+                Button(action: {
+                    self.isEditing = false
+                    self.text = ""
+                    
+                    // Dismiss the keyboard
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                }) {
+                    Text("Cancel")
+                }
+                .padding(.trailing, 10)
+                .transition(.move(edge: .trailing))
+                
+            }
+        }
+        .padding(.top, isEditing ? 0: 30)
+        .animation(.easeInOut, value: isEditing)
+    }
+}
+
+#Preview {
+    SearchBar(text: .constant("test"))
+}
