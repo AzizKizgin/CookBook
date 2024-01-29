b03b19e23550ed8c3c93a13625ff768bda9bda3a
Aziz Kızgın <azizkzgn@gmail.com>
2024-01-29 20:49:30 +0300

    feat: SearchView and SearchViewModel added

--- Cookbook/Views/Search/SearchView.swift
+++ Cookbook/Views/Search/SearchView.swift
@@ -8,11 +8,53 @@
 import SwiftUI
 
 struct SearchView: View {
+    @StateObject var searchVM = SearchViewModel()
+    // I'm using this state for searchbar bug which occurs with tabview
+    @State var showList: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            if showList {
+                List{
+                    ForEach(searchVM.meals, id: \.id) { meal in
+                        NavigationLink(value: meal) {
+                            Text(meal.name)
+                        }
+                    }
+                }
+            }
+        }
+        .overlay{
+            if searchVM.meals.isEmpty {
+                VStack{
+                    Image(systemName: "doc.text.magnifyingglass")
+                        .font(.system(size: 150))
+                    Text("Search Meal")
+                        .font(.system(size: 30))
+                        .bold()
+                }
+                .foregroundStyle(.accent)
+            }
+        }
+        .searchable(text: $searchVM.searchText,
+               placement: .navigationBarDrawer(displayMode: .always))
+        .onChange(of: searchVM.searchText) { _, newValue in
+            if !newValue.isEmpty {
+                searchVM.searchMeal()
+            } else {
+                searchVM.meals = []
+            }
+        }
+        .onAppear {
+            showList = true
+        }
+        .navigationDestination(for: Meal.self) { meal in
+            MealDetailView(mealId: meal.id)
+        }
     }
 }
 
 #Preview {
-    SearchView()
+    NavigationStack{
+        SearchView()
+    }
 }
--- Cookbook/Views/Search/SearchViewModel.swift
+++ Cookbook/Views/Search/SearchViewModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+class SearchViewModel: ObservableObject {
+    @Published var isLoading: Bool = false
+    @Published var meals: [Meal] = []
+    @Published var searchText: String = ""
+    @Published var selectedMeal: Meal?
+    
+    func searchMeal() {
+        self.isLoading = true
+        MealMenager.shared.getMealsByName(name: searchText) { [weak self] meals in
+            DispatchQueue.main.async {
+                if let meals {
+                    self?.meals = meals
+                    self?.isLoading = false
+                }
+            }
+        }
+    }
+}
