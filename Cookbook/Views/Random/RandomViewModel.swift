926f68f242170a9ed171c7b071976c4410f8c9fa
Aziz Kızgın <azizkzgn@gmail.com>
2024-02-01 02:08:16 +0300

    feat: getRandomMeal updated and refresh added

--- Cookbook/Views/Random/RandomViewModel.swift
+++ Cookbook/Views/Random/RandomViewModel.swift
@@ -9,16 +9,25 @@ import Foundation
 class RandomViewModel: ObservableObject {
     @Published var isLoading: Bool = false
     @Published var meal: Meal?
+    @Published var dataLoaded: Bool = false
     
     func getRandomMeal() {
-        self.isLoading = true
-        MealMenager.shared.getRandomMeal() { [weak self] meal in
-            DispatchQueue.main.async {
-                if let meal {
-                    self?.meal = meal
-                    self?.isLoading = false
+        if !dataLoaded {
+            self.isLoading = true
+            MealMenager.shared.getRandomMeal() { [weak self] meal in
+                DispatchQueue.main.async {
+                    if let meal {
+                        self?.meal = meal
+                        self?.isLoading = false
+                        self?.dataLoaded = true
+                    }
                 }
             }
         }
+    }
+    
+    func refresh(){
+        self.dataLoaded = false
+        self.getRandomMeal()
     }
 }
