da2d8cd102af9116e7029a377276e9a7c2630d37
Aziz Kızgın <azizkzgn@gmail.com>
2024-02-01 02:08:36 +0300

    feat: dark mode toggle added

--- Cookbook/Views/Settings/SettingsView.swift
+++ Cookbook/Views/Settings/SettingsView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @AppStorage("isDark") private var isDark: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List{
+            Toggle(isOn: $isDark, label: {
+                Text("Dark Mode")
+            })
+        }
     }
 }
 
